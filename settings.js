ab038aad94e840075db10e0dc40c2cf4c702a771
nolove2005 <116479343+nolove2005@users.noreply.github.com>
2022-10-24 13:22:47 +0530

    SACHIYA

--- settings.js
+++ settings.js
@@ -19,18 +19,18 @@ global.APIKeys = {
 }
 
 //customize settings\\
-global.owner = ['94711421243']
-global.premium = ['94711421243']
-global.ownernomer = '94711421243'
-global.ownername = 'Dark Alpha'
-global.botname = 'BIXBY-MD USER BOT'
-global.footer = ' Qᴜᴇᴇɴ ʙɪxʙʏ ᴘᴜʙʟɪᴄ ᴇᴅɪᴛɪᴏɴ.'
+global.owner = ['94767233346']
+global.premium = ['94767233346']
+global.ownernomer = '94767233346'
+global.ownername = '𝐒𝐀𝐂𝐇𝐈𝐘𝐀 𝐌𝐃'
+global.botname = '𝐒𝐀𝐂𝐇𝐈𝐘𝐀 𝐌𝐃'
+global.footer = ' 𝐒𝐀𝐂𝐇𝐈𝐘𝐀 𝐌𝐃.'
 global.ig = 'https://github.com/darkalphaxteam'
 global.region = 'India, South India, Kerala'
 global.sc = 'https://github.com/darkalphaxteam/QUEEN-BIXBY-MD-PRO-NEW'
 global.myweb = 'https://youtube.com/channel/UCvpyOCBCaBSCdv4QolnO0WQ'
-global.packname = 'Queen Bixby MD'
-global.author = 'DARK ALPHA'
+global.packname = '𝐒𝐀𝐂𝐇𝐈𝐘𝐀 𝐌𝐃'
+global.author = '𝐒𝐀𝐂𝐇𝐈𝐘𝐀 𝐌𝐃'
 global.sessionName = 'session'
 global.prefa = ['','!','.','🐦','🐤','🗿']
 global.sp = '🚢'
